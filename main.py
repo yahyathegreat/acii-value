28b7b19c6a4b6971d2201ec96d176f3ffd30ab29
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-14 19:50:40 +0500

    power calc

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("ASCII value of given character is ", ord(ch))
 n = int(input("enter a number"))
 x = bin(n)
 print(x)
+n = int(input("enter a number"))
+for i in range (0,n):
+    print(i**i )
